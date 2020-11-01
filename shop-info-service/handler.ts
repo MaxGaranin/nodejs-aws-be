6a79ee0f18a0dfc8978326b86e96a29cf883fa7c
Max Garanin <maxgaranin@gmail.com>
2020-11-01 12:46:36 +0400

    feat: remove lecture samples

--- shop-info-service/handler.ts
+++
@@ -1,6 +0,0 @@
-import 'source-map-support/register';
-
-import { getShopAdminInfo } from './handlers/getShopAdminInfo';
-import { getShopInfo } from './handlers/getShopInfo';
-
-export {getShopAdminInfo, getShopInfo};
