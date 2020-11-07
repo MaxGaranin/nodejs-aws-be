e1877a695058a42e90b424f89651eae25a66db20
Max Garanin <maxgaranin@gmail.com>
2020-11-07 23:30:00 +0400

    feat: setup API Gateway for pg-client-lambda

--- product-service/handlers/pg-client-lambda.ts
+++ product-service/handlers/pg-client-lambda.ts
@@ -1,34 +1,50 @@
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import 'source-map-support/register';
+
 import { Client } from 'pg';
+import { corsHeaders } from './constants';
 
 const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
 
 const dbOptions = {
-    host: PG_HOST,
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USERNAME,
-    password: PG_PASSWORD,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectionTimeoutMillis: 5000
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
 };
 
-export const pgGetProducts = async event => {
-    const client = new Client(dbOptions);
-    await client.connect();
+export const pgGetProducts: APIGatewayProxyHandler = async () => {
+  const client = new Client(dbOptions);
+  await client.connect();
 
-    try {
-        const { rows: products } = await client.query(`select * from products`);
-        console.log(products);
+  try {
+    const { rows: products } = await client.query(`select * from products`);
+    console.log(products);
 
-        console.log('-------------');
-        console.log(PG_HOST);
-    }
-    catch (err) {
-        console.log('Error during database request operations', err);
-    }
-    finally {
-        client.end();
-    }
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: JSON.stringify({
+        products,
+      }),
+    };
+  } catch (e) {
+    return errorFetchProducts(e);
+  } finally {
+    client.end();
+  }
 };
+
+function errorFetchProducts(e: { message: any; }): Promise<APIGatewayProxyResult> {
+  return Promise.resolve({
+    statusCode: 500,
+    body: JSON.stringify({
+      message: `error then fetch products: ${e.message}`,
+    }),
+  });
+}
