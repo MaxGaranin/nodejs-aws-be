21e6e72c1d263e13262988c2fbf03316a26eac26
Max Garanin <maxgaranin@gmail.com>
2020-11-16 12:53:21 +0400

    feat: rework importFileParser lambda function

--- import-service/handlers/importFileParser.ts
+++ import-service/handlers/importFileParser.ts
@@ -1,69 +1,72 @@
-import 'source-map-support/register';
+import csv from 'csv-parser';
+import S3 from 'aws-sdk/clients/s3';
+import { S3Event } from 'aws-lambda';
 
-const AWS = require('aws-sdk');
-const csv = require('csv-parser');
 const BUCKET = 'rss-aws-task5';
 
-export const importFileParser = async (event) => {
+export const importFileParser = async (event: S3Event) => {
   console.log('Lambda function importFileParser has invoked');
   console.log('event: ', event);
 
-  const s3 = new AWS.S3({ region: 'eu-west-1' });
+  const s3 = new S3({ region: 'eu-west-1' });
 
-  event.Records.forEach(async (record) => {
-    console.dir(record);
+  try {
+    for (const record of event.Records) {
+      const { key } = record.s3.object;
 
-    const s3Stream = s3
-      .getObject({
-        Bucket: BUCKET,
-        Key: record.s3.object.key,
-      })
-      .createReadStream();
+      const s3Stream = s3
+        .getObject({
+          Bucket: BUCKET,
+          Key: key,
+        })
+        .createReadStream();
 
-    try {
       await new Promise((resolve, reject) => {
-        console.log(`------------in promise----------`);
         s3Stream
           .pipe(csv())
           .on('data', (data) => {
             console.log(data);
           })
           .on('error', (error) => {
-            console.log(`------------error----------`);
             reject(error);
           })
           .on('end', async () => {
-            console.log(`------------end----------`);
-            const uploadedPath = `${BUCKET}/${record.s3.object.key}`;
-            const parsedPath = record.s3.object.key.replace(
-              'uploaded',
-              'parsed'
-            );
-
-            console.log(`Copy from ${uploadedPath}`);
+            console.log(`Copy from ${BUCKET}/${key}`);
 
             await s3
               .copyObject({
                 Bucket: BUCKET,
-                CopySource: uploadedPath,
-                Key: parsedPath,
+                CopySource: `${BUCKET}/${key}`,
+                Key: key.replace('uploaded', 'parsed'),
               })
               .promise();
 
-            console.log(`Copied into ${parsedPath}`);
+            console.log(
+              `Copy to ${BUCKET}/${key.replace('uploaded', 'parsed')}`
+            );
+
+            await s3
+              .deleteObject({
+                Bucket: BUCKET,
+                Key: key,
+              })
+              .promise();
+
+            console.log(`Delete ${BUCKET}/${key}`);
 
             resolve();
           });
       });
-    } catch (error) {
-      return {
-        statusCode: 500,
-        message: error.message,
-      };
     }
-  });
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: error.message,
+    };
+  }
 
   return {
     statusCode: 202,
+    message: 'File was copied and parsed',
   };
 };
